f39dabeab206a7594800b1f8bb1264b9fa270c56
Carsten <admin@AR-113-04>
2017-04-27 15:07:05 +0200

    Første version

---
+++ Opg01_HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opg01_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Indtast navn
+            Console.WriteLine("Indtast dit navn");
+
+            String Navn = Console.ReadLine();
+
+            //Oversæt til store bogstaver
+            String NavnStoreBogstaver = Navn.ToUpper();
+
+            Console.WriteLine("Du indtastede: " + Navn);
+            Console.WriteLine("Oversat til store bogstaver: " + NavnStoreBogstaver);
+
+            Console.ReadKey();
+        }
+    }
+}
