6a3d19d145e3f2ed38fa4f7d3fd083f7d997dcc7
Carsten <admin@AR-113-04>
2017-04-28 13:18:58 +0200

    Opgave 3 tilføjet

---
+++ COpg03Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COpg03Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] maanedsloen = new int[12];
+            double average = 0;
+            double sum = 0;
+
+            for (int i = 0; i < maanedsloen.Length; i++)
+            {
+                maanedsloen[i] = 15000 + (i * 500);
+                sum = sum + maanedsloen[i];
+                Console.WriteLine("Månedsløn: " + i + " " + maanedsloen[i]);
+
+            }
+
+            Console.WriteLine("Sum månedsløn: " + sum);
+            Console.WriteLine("Gennemsnitlig månedsløn: " + (sum / maanedsloen.Length));
+
+            Console.ReadKey();
+
+        }
+    }
+}
