20b217bb6c1b31cea992aa67b7a0967fa747cc3f
Carsten <admin@AR-113-04>
2017-05-08 11:07:46 +0200

    Omstruktureret

--- Opg6aBeregnArray/Program.cs
+++ Opg6aBeregnArray/Program.cs
@@ -19,8 +19,10 @@ namespace Opg6aBeregnArray
             int[] test = { 10, 7, 6, 3, 1, 50, 3 };
             var res = BeregnOgSortArray(test);
 
-            Console.WriteLine("Sum:" + res.sum);
-            Console.WriteLine("Gennemsnit:" + res.gennemsnit);
+            Udskriv();  // blank linje
+            Udskriv("Sum:" + res.sum);
+            Udskriv();  // blank linje
+            Udskriv("Gennemsnit:" + res.gennemsnit);
 
         }
         static ArrayRes BeregnOgSortArray(int[] test) 
@@ -29,22 +31,41 @@ namespace Opg6aBeregnArray
             double gns = 0;
             ArrayRes lc_arrayres;
 
+            Udskriv("FØR sortering"); // Dette er en test
             for (int i = 0; i < test.Length; i++)
             {
                 sum = sum + test[i];
+                Udskriv("Tal i TEST: " + i + " " + test[i]);
             }
 
             gns = sum / test.Length;
             lc_arrayres.sum = sum;
             lc_arrayres.gennemsnit = gns;
 
+            Udskriv();  // blank linje
+
             // Sorter tallene
             Array.Sort(test);
+
+            Udskriv("EFTER sortering"); // Dette er en test
+            for (int i = 0; i < test.Length; i++)
+            {
+                Udskriv("Tal i TEST: " + i + " " + test[i]);
+
+            }
 
             return lc_arrayres;
 
 
 
         }
+
+        //Udskriv tekst, hvis ingen værdi overføres er default = Blank!!
+        static void Udskriv(string tekst = "")
+        {
+            Console.WriteLine(tekst);
+        }
+
+
     }
 }
