77e6c03975de7d9b51938f9d20e59391079436d4
Carsten <admin@AR-113-04>
2017-05-09 11:11:53 +0200

    Person tilføjet

---
+++ Modul07Person/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul07Person
+{
+    class Person
+    {
+        public string Fornavn { get; set; }
+
+        private string efternavn;
+
+        public string Efternavn
+        {
+            get { return efternavn; }
+
+            set
+            {
+                if (value.Length < 4)
+                {
+                    efternavn = "";
+                }
+                else
+                {
+                    efternavn = value;
+                }
+            }
+        }
+
+        private string fuldenavn;
+
+        public string FuldeNavn
+        {
+            get {
+                fuldenavn = this.Fornavn + " " + this.Efternavn;
+                return fuldenavn;
+            }
+            //set { fuldenavn = value; }
+        }
+
+
+        public string FuldtNavn()
+        {
+            return this.Fornavn + " " + this.efternavn;
+
+        }
+    }
+}
---
+++ Modul07Person/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul07Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+
+            person1.Fornavn = "Carsten";
+            person1.Efternavn = "Sørensen";
+
+            Console.WriteLine("FuldtNavn (Metode) = " + person1.FuldtNavn());
+            Console.WriteLine("FuldeNavn (property) = " + person1.FuldeNavn);
+        }
+    }
+}
