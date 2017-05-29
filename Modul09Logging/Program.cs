80b9b97c8aea326a4f9464bcff44a7c22715a308
Carsten <fonkel@gmail.com>
2017-05-29 13:28:36 +0200

    Logging program tilføjet

---
+++ Modul09Logging/Program.cs
@@ -0,0 +1,49 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul09Logging
+{
+    class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            logger.Trace("Her starter vi");
+            logger.Debug("Kald af Test1");
+            test1(5, 5);
+            logger.Debug("Efter Test1");
+
+            logger.Debug("Kald af Test2");
+            test2();
+            logger.Debug("Efter Test2");
+
+            logger.Trace("Her slutter vi så");
+        }
+
+        static void test1(int i, int x)
+        {
+            logger.Trace("Nu er vi nede i test1, i = " + i + " x = " + x);
+            Console.WriteLine("test1");
+            System.Threading.Thread.Sleep(3000);
+        }
+
+        static void test2()
+        {
+            try
+            {
+                logger.Trace("Nu er vi nede i test2");
+                Console.WriteLine("test2");
+                throw new ApplicationException("fejl");
+            }
+            catch (Exception ex)
+            {
+                // mangler log
+            }
+        }
+    }
+}
---
+++ Module09logging/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+
+namespace Module09logging
+{
+    class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+        static void Main(string[] args)
+        {
+            test1(5, 5);
+            test2();
+        }
+
+        static void test1(int i, int x)
+        {
+            Console.WriteLine("test1");
+            System.Threading.Thread.Sleep(3000);
+        }
+
+        static void test2()
+        {
+            try
+            {
+                Console.WriteLine("test2");
+                throw new ApplicationException("fejl");
+            }
+            catch (Exception ex)
+            {
+                // mangler log
+            }
+        }
+    }
+}
