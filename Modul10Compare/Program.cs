078db8d0938eda0a57db12cc6acb683440c2e33c
Carsten <fonkel@gmail.com>
2017-05-30 09:27:35 +0200

    Sortering på Navn

--- Modul10Compare/Program.cs
+++ Modul10Compare/Program.cs
@@ -20,8 +20,8 @@ namespace Modul10Compare
             logger.Trace("Her starter vi");
 
             hunde[0] = new Hund() { Alder = 10, Navn = "Rollo" };
-            hunde[1] = new Hund() { Alder = 7, Navn = "King" };
-            hunde[2] = new Hund() { Alder = 15, Navn = "Old Oscar" };
+            hunde[1] = new Hund() { Alder = 7, Navn = "Abott" };
+            hunde[2] = new Hund() { Alder = 15, Navn = "King" };
 
             logger.Trace("Her sorterer vi");
             Array.Sort(hunde);
@@ -43,18 +43,27 @@ namespace Modul10Compare
             public int Alder { get; set; }
 
             public int CompareTo(object obj)
-            {
-                Hund h = obj as Hund;
-                if (h.Alder < this.Alder)
-                    return -1;
-                if (h.Alder > this.Alder)
-                    return 1;
+            //{
+            //    Hund h = obj as Hund;
+            //    if (h.Alder < this.Alder)
+            //        return -1;
+            //    if (h.Alder > this.Alder)
+            //        return 1;
 
-                return 0;
-            }
-                //public int CompareTo(object obj)
-                //{
-                //    Hund h = obj as Hund;
+            //    return 0;
+            //}
+            //public int CompareTo(object obj)
+            {
+                int i = 0;
+                Hund h = obj as Hund;
+
+                i = string.Compare(h.Navn, this.Navn);
+
+                // Sorter stigende på navn
+                i = i * -1;
+
+                return i;
+                // return string.Compare(h.Navn, this.Navn);
                 //    if (h.Navn < this.Navn)
                 //        return -1;
                 //    if (h.Navn > this.Navn)
@@ -62,8 +71,8 @@ namespace Modul10Compare
 
                 //    return 0;
 
-                //}
-                //}
             }
+            //}
+        }
     }
 }
