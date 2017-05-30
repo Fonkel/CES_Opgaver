f333af1ace860a831e32541b85f780bc1f36d118
Carsten <fonkel@gmail.com>
2017-05-30 13:13:04 +0200

    Delegate tilføjet

---
+++ Modul11Delegate/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul11Delegate
+{
+    class Program
+    {
+
+        public delegate int BeregnDelegate(int a, int b);
+
+        static void Main(string[] args)
+        {
+            int resultat = beregner(1, 1, plus);
+            Console.WriteLine(resultat);
+
+            resultat = beregner(1, 1, minus);
+            Console.WriteLine(resultat);
+
+            resultat = beregner(2, 3, potens);
+            Console.WriteLine(resultat);
+
+        }
+
+        public static int beregner(int a, int b, BeregnDelegate funktion)
+        {
+            return funktion(a, b);
+        }
+
+        //PLUS beregning
+        public static int plus(int a, int b)
+        {
+            Console.WriteLine(a + " + " + b + " = ");
+            return a + b;
+        }
+
+        //MINUS beregning
+        public static int minus(int a, int b)
+        {
+            Console.WriteLine(a + " - " + b + " = ");
+            return a - b;
+        }
+
+        //DIVIDER beregning
+        public static int divider(int a, int b)
+        {
+            Console.WriteLine(a + " / " + b + " = ");
+            return a / b;
+        }
+
+        //GANGE beregning
+        public static int gange(int a, int b)
+        {
+            Console.WriteLine(a + " * " + b + " = ");
+            return a * b;
+        }
+
+        //POTENS beregning
+        public static int potens(int a, int b)
+        {
+            int p2 = 1;
+
+            Console.WriteLine(a + " ** " + b + " = ");
+
+            for (int ix1 = 1; ix1 <= b; ix1++)
+
+            {
+                p2 = p2 * a;
+            }
+
+
+            return p2;
+        }
+    }
+}
