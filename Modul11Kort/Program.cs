ca332be03ba4c0409ce57c039bc9b2e4ea3f7397
Carsten <fonkel@gmail.com>
2017-05-30 10:58:51 +0200

    Kortbunke tilføjet

---
+++ Modul11Kort/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul11Kort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Bunke kortbunke = new Bunke();
+            kortbunke.TilFøjkort(new Kort() { Kulør = "Spar", Værdi = 7 });
+            kortbunke.TilFøjkort(new Kort() { Kulør = "Hjerter", Værdi = 10 });
+            kortbunke.TilFøjkort(new Kort() { Kulør = "Ruder", Værdi = 1 });
+            kortbunke.TilFøjkort(new Kort() { Kulør = "klør", Værdi = 5 });
+
+            kortbunke.vis();
+            Console.WriteLine("");
+            Console.WriteLine("Et kort trækkes fra bunken");
+            Console.WriteLine("");
+
+            var kort = kortbunke.Fjernkort();
+            Console.WriteLine("");
+            Console.WriteLine("Kortet var: " + kort);
+
+            Console.WriteLine("");
+            Console.WriteLine("Og bunken indeholder nu:");
+            kortbunke.vis();
+
+
+            Console.WriteLine("***");
+            Console.WriteLine("Tryk en taste");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Modul11Kort/bunke.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul11Kort
+{
+    class Bunke
+    {
+        private Stack<Kort> bunke = new Stack<Kort>();
+        public void TilFøjkort(Kort k)
+        {
+            bunke.Push(k);
+        }
+        public Kort Fjernkort()
+        {
+            if (bunke.Count > 0)
+                return bunke.Pop();
+
+            return null;
+        }
+
+        public void vis()
+        {
+            foreach (var kort in bunke)
+            {
+                Console.WriteLine(kort);
+            }
+        }
+    }
+}
